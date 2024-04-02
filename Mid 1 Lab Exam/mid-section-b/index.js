ff2e9274fee23a75a3f649983eb6c31e7c4bf766
AishaAmin24 <120329171+AishaAmin24@users.noreply.github.com>
2024-04-02 15:03:11 +0500

    Add files via upload

---
+++ Mid 1 Lab Exam/mid-section-b/index.js
@@ -0,0 +1,19 @@
+const image = ["./one.svg" , "image1/","./three.svg","image2/ ","./four.svg","image2/ "];
+const con= document.querySelector("con");
+const imgnext= document.querySelector('Next');
+const imgprev= document.querySelector('Prev');
+
+let total =0;
+
+Next.addEventListner("click", () => {
+if(total<=1){
+    image = $img[count]
+    console.log()
+}
+else{
+    con.innerHTML = "Empty";
+}
+}
+let count = 
+
+
